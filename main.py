aa4710bc39cd7be9e4ac80f4ffc978dfd3add260
SharkSpidy <josephshibuwork@gmail.com>
2025-05-01 22:56:49 +0530

    Hit Bug

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ img2 = cv2.imread("Train/queen.jpg")
 
 img2_resized = cv2.resize(img2, (0, 0), fx=0.2, fy=0.2)
 
+orb = cv2.ORB_create()
+
+
 
 cv2.imshow("img1", img1)
 cv2.imshow("img2", img2_resized)
