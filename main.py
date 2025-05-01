8a1c5ac3acd16413c21519d6b54d0bff8bae2a7b
SharkSpidy <josephshibuwork@gmail.com>
2025-05-01 22:00:23 +0530

    Altered Images

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 img1 = cv2.imread("Images/qoc.jpg")
 img2 = cv2.imread("Train/queen.jpg")
 
+img2_resized = cv2.resize(img2, (0, 0), fx=0.2, fy=0.2)
+
+
 cv2.imshow("img1", img1)
-cv2.imshow("img2", img2)
+cv2.imshow("img2", img2_resized)
 cv2.waitKey(0)
