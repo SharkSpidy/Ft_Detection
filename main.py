4ef4328e5aec8f3ca100a6e5a0b185403881edb6
SharkSpidy <josephshibuwork@gmail.com>
2025-05-01 21:52:09 +0530

    Added Images For Training

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from ft-detection!")
+import cv2
+import numpy as np
 
+img1 = cv2.imread("Images/qoc.jpg")
+img2 = cv2.imread("Train/queen.jpg")
 
-if __name__ == "__main__":
-    main()
+cv2.imshow("img1", img1)
+cv2.imshow("img2", img2)
+cv2.waitKey(0)
